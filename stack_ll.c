0199cd77a67b5bf68272a70ccd28867818b836a5
Aditya Potluri <adityapotluri1@gmail.com>
2020-07-28 19:49:48 -0400

    unfinished linked list stack

---
+++ stack_ll.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdbool.h>
+#include<limits.h>
+#define MAX_LENGTH 5
+#define EMPTY -1
+
+typedef struct node{
+    int val;
+    node* next;
+} node;
+
+
+node stack;
+
+bool push(int val) {
+    if(stack)
+}
+
+
+
